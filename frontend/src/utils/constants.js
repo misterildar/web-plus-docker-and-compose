7f27df521a37ec49fba3ea9ce99d864b32d7b9ad
Ildar <misterildar@gmail.com>
2024-03-25 14:32:46 +0300

    deploy2

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://api.kpd.ildar.nomoredomainswork.ru/";
+export const URL = "https://api.kpd.ildar.nomoredomainswork.ru/";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
